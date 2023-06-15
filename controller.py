e3da38f9313844de288400816c670ce9ea4c240a
Ricardo Souza <rl03souza@gmail.com>
2023-06-15 17:47:06 -0300

    Feat: create player class and add player to screen

--- controller.py
+++ controller.py
@@ -1,8 +1,19 @@
 import pygame
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, screen_size) -> None:
+        super().__init__()
+        self.x = screen_size[0]/2
+        self.y = screen_size[1]/2
+        self.screen = (screen_size[0], screen_size[1])
+        self.image = pygame.Surface((60,60))
+        self.image.fill((225, 225, 225))
+        self.rect = self.image.get_rect(center = (self.x, self.y))
+
 class Controller():
     def __init__(self) -> None:
         self.running = True
+        self.clock = pygame.time.Clock()
         self.screen_width = 800
         self.screen_heigth = 500
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_heigth))
@@ -10,10 +21,18 @@ class Controller():
     def start(self):
         pygame.init()
         pygame.display.set_caption("Shooter game")
+        player = Player((self.screen_width, self.screen_heigth))
+        player_group = pygame.sprite.Group()
+        player_group.add(player)
+
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+
+            player_group.draw(self.screen)
+            pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == "__main__":
     Controller().start()
