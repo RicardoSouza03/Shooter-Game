ab85d5648eb32d65906e35a309b679f5cb16df56
Ricardo Souza <rl03souza@gmail.com>
2023-07-19 17:31:22 -0300

    Feat: add option to reset animation

--- utils/animation.py
+++ utils/animation.py
@@ -1,11 +1,13 @@
 import pygame
 
 class Animation(pygame.sprite.Sprite):
-    def __init__(self, animation_sheet, steps, speed, width, height, scale) -> None:
+    def __init__(self, animation_sheet, steps, speed, width, height, scale, loop) -> None:
         super().__init__()
         self.sheet_image = pygame.image.load(animation_sheet).convert_alpha()
         self.frame = 0
-        self.ended = True
+        self.ended = False
+        self.reset = True
+        self.loop = loop
         self.animation_speed = speed
         self.steps = steps
         self.width = width
@@ -25,14 +27,32 @@ class Animation(pygame.sprite.Sprite):
 
         return image
     
-    def update(self):
-        current_time = pygame.time.get_ticks()
-        self.ended = False
-        self.image = self.animation_list[self.frame]
+    def reset_animation(self, is_clicked):
+        if not self.reset: 
+            self.ended = True
+            self.kill()
 
+        if not self.ended and is_clicked:
+            self.frame = len(self.animation_list) - 1
+        elif self.ended:
+            self.frame = 0
+
+        if self.loop: self.frame = 0
+        
+    def update(self):
+        is_clicked = pygame.mouse.get_pressed()
+
+        current_time = pygame.time.get_ticks()
+        self.image = self.animation_list[self.frame]
         if current_time - self.last_update >= self.animation_speed:
             self.frame += 1
             self.last_update = current_time
+
         if self.frame >= self.steps:
-            self.ended = True
-            self.frame = 0
+            self.reset_animation(is_clicked)
+
+    def set_ended(self, should_end):
+        self.ended = should_end
+
+    def set_reset(self, reset):
+        self.reset = reset
