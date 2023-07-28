9e32596b5bbdc5f11c0312834c61e09da74c1605
Ricardo Souza <rl03souza@gmail.com>
2023-07-28 15:12:44 -0300

    Refactor: menu_section to show price if locked

--- utils/Menu_section.py
+++ utils/Menu_section.py
@@ -1,12 +1,13 @@
-import pygame
 from utils.button import Button
 from utils.load_image import load_image
+from utils.display_text import display_text
 
 class MenuOptionsSection():
-    def __init__(self, menu_dict, curr_skin, screen):
+    def __init__(self, menu_dict, curr_skin, screen, best_score):
         self.menu_dict = menu_dict
         self.screen = screen
         self.current_skin = curr_skin
+        self.best_score = best_score
 
     def change_selected_skin(self):
         for character in self.menu_dict:
@@ -20,19 +21,29 @@ class MenuOptionsSection():
         for key in self.menu_dict:
             self.change_selected_skin()
 
-            if self.menu_dict[key]['is_unlocked']:
+            if self.best_score >= self.menu_dict[key]['price']:
                 key_button = Button(self.menu_dict[key]['coordinate_x'], self.menu_dict[key]['coordinate_y'], self.menu_dict[key]['image'])
+                price_text_info = {'text': '', 'coordinate': (0,0), 'font_size': 0}
             else:
                 key_button = Button(self.menu_dict[key]['coordinate_x'], self.menu_dict[key]['coordinate_y'], self.menu_dict[key]['image_locked'])
+                price_text_info = {
+                    'text': self.menu_dict[key]['price'], 
+                    'coordinate': (self.menu_dict[key]['coordinate_x']+ 30, self.menu_dict[key]['coordinate_y']+105), 
+                    'font_size': 30
+                }
 
-            buttons_list.append({'skin_name': key, 'button': key_button, 'is_enabled': self.menu_dict[key]['is_unlocked']})
+            buttons_list.append({'skin_name': key, 'button': key_button, 'is_enabled': self.best_score >= self.menu_dict[key]['price'], 'price_text': price_text_info})
         return buttons_list
     
     def draw_buttons(self):
         buttons_list = self.create_dict_buttons()
         for button in buttons_list:
-            if button['button'].draw(self.screen) and button['is_enabled']:
-                self.current_skin = button['skin_name']
+            if button['is_enabled']:
+                if button['button'].draw(self.screen):
+                    self.current_skin = button['skin_name']
+            else:
+                button['button'].draw(self.screen)
+                display_text(self.screen, button['price_text']['text'], button['price_text']['coordinate'], button['price_text']['font_size'])
         
     def draw_menu(self):
         self.draw_buttons()
